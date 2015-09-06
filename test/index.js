508270eb5f6ae3f86c6ac16dd64dfa654b44421c
Stefan Grönke <stefan@gronke.net>
2015-09-06 04:48:49 +0200

    test using path prefixes

--- test/index.js
+++ test/index.js
@@ -22,7 +22,8 @@ describe('express-force-domain middleware', function() {
 
     app.use(forceDomain('example.com'))
 
-    request('wrong.example.com', function(res) {
+    request('wrong.example.com')
+    .then(function(res) {
 
       expect(res).to.exist;
       expect(res.statusCode).to.be.equal(301);
@@ -36,7 +37,7 @@ describe('express-force-domain middleware', function() {
         done();
       });
 
-    })
+    });
 
   });
 
@@ -48,24 +49,77 @@ describe('express-force-domain middleware', function() {
       res.end();
     });
 
-    request('example.com', function(res) {
+    request('example.com')
+    .then(function(res) {
 
       expect(res).to.exist;
       expect(res.statusCode).to.be.equal(200);
       done();
 
+    });
+
+  });
+
+  it('can be used with path prefixes', function(done) {
+
+    app.get('/baz', function(req, res) {
+      res.sendStatus(200);
+      res.end();
+    });
+
+    app.use('/foo', forceDomain('example.com'));
+    app.get('/foo/bar', function(req, res) {
+      res.sendStatus(200);
+      res.end();
+    });
+
+    request('example.com', '/foo/bar')
+    .then(function(res) {
+      expect(res).to.exist;
+      expect(res.statusCode).to.be.equal(200);
+
+      return request('example.com', '/baz');
+    }).then(function(res) {
+      expect(res).to.exist;
+      expect(res.statusCode).to.be.equal(200);
+
+      return request('wrong.example.com', '/foo/bar');
     })
+    .then(function(res) {
+      expect(res).to.exist;
+      expect(res.statusCode).to.be.equal(301);
+
+      return request('wrong.example.com', '/baz');
+    })
+    .then(function(res) {
+      expect(res).to.exist;
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
 
   });
 
 });
 
-function request(hostname, callback) {
+function request(hostname, path, callback) {
+
+  path = path || '/';
+
+  var q = new Promise(function(resolve, reject) {
+    if(!callback) {
+      callback = resolve;
+    }
+  });
+
   http.get({
     port: serverPort,
     hostname: '127.0.0.1',
+    path: path,
     headers: {
       host: hostname
     }
   }, callback);
+
+  return q;
+
 }
